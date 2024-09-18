cf2c71f05aca57aff7397d6cd5ad30a2f7c8a6a9
Chaitanya Mishra <mishrachaitanya.35@gmail.com>
2024-09-18 14:20:36 +0530

    Latest

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info().exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -6,10 +8,10 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 
-class CustomeException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super.__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
     def __str__(self):
         return self.error_message
+
